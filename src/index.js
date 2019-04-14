89a6ab4700c57b00f3cd37b4c20a72c84b2a73de
Steven Roberts <contact@stevenroberts.info>
2019-04-14 09:07:29 +0100

    added socket.io

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 const   path = require('path'),
+        http = require('http'),
         express = require('express'),
-        app = express(),
-        port = process.env.PORT || 3000,
+        socketio = require('socket.io');
+
+
+const   app = express(),
+        server = http.createServer(app),
+        io = socketio(server);
+
+const   port = process.env.PORT || 3000,
         publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+
+io.on('connection', () => {
+    console.log('New Websocket connection');
+});
+
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 })
 
